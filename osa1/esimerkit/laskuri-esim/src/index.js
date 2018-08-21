0b82e9ab11a46b7704f1d739b3db4788bcc6cbd8
avaruustursas <34242717+avaruustursas@users.noreply.github.com>
2018-08-21 03:17:51 +0300

    laskuri-esim talteen ennen ajastimen lisäystä

--- osa1/esimerkit/laskuri-esim/src/index.js
+++ osa1/esimerkit/laskuri-esim/src/index.js
@@ -1,28 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = (props) => {
-  const {counter} = props
-  return (
-    <div>{counter.value}</div>
-  )
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      counter: 1
+    }
+  }
+
+  render() {
+    return (
+      <div>{this.state.counter}</div>
+    )
+  }
 }
 
-const counter = {
-  value: 1
-}
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
+// const App = (props) => {
+//   const {counter} = props
+//   return (
+//     <div>{counter.value}</div>
+//   )
+// }
 
-const renderoi = () => {
-  ReactDOM.render(
-    <App counter={counter} />,
-    document.getElementById('root')
-  )
-}
+// const counter = {
+//   value: 1
+// }
 
-setInterval(() => {
-  renderoi()
-  counter.value += 1;
-}, 1000)
+// const renderoi = () => {
+//   ReactDOM.render(
+//     <App counter={counter} />,
+//     document.getElementById('root')
+//   )
+// }
+//
+// setInterval(() => {
+//   renderoi()
+//   counter.value += 1;
+// }, 1000)
 // renderoi()
 // counter.value += 1
 // renderoi()

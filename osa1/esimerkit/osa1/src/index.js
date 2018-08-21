acb2eb3e1796cbdd778fbafce46f08877ac75066
avaruustursas <34242717+avaruustursas@users.noreply.github.com>
2018-08-21 03:12:54 +0300

    Testattiin luokkajuttuja esimerkissä

--- osa1/esimerkit/osa1/src/index.js
+++ osa1/esimerkit/osa1/src/index.js
@@ -28,13 +28,77 @@ import ReactDOM from 'react-dom';
 //   )
 // }
 
-const Hello = (props) => {
-  return (
-    <div>
-      <p>Hello {props.name}, you are {props.age} years old</p>
-    </div>
-  )
+
+//destrukturointi
+class Hello extends React.Component {
+  render() {
+    const {name,age} = this.props
+    const bornYear = () => new Date().getFullYear() - age
+    return (
+      <div>
+        <p>
+          Hello {name}, you are {age} years old<br />
+          So you were probably born {bornYear()}
+        </p>
+      </div>
+    )
+  }
 }
+//
+// class Hello extends React.Component {
+//   render() {
+//     const name = this.props.name
+//     const age = this.props.age
+//     const bornYear = () => new Date().getFullYear() - age
+//     return (
+//       <div>
+//         <p>
+//           Hello {name}, you are {age} years old<br />
+//           So you were probably born {bornYear()}
+//         </p>
+//       </div>
+//     )
+//   }
+// }
+// class Hello extends React.Component {
+//   render() {
+//     const bornYear = () => {
+//       const yearNow = new Date().getFullYear()
+//       return yearNow - this.props.age
+//     }
+//     return (
+//       <div>
+//         <p>
+//           Hello {this.props.name}, you are {this.props.age} years old<br />
+//           So you were probably born {bornYear()}
+//         </p>
+//       </div>
+//     )
+//   }
+// }
+// class Hello extends React.Component {
+//   bornYear() {
+//     const yearNow = new Date().getFullYear()
+//     return yearNow - this.props.age
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <p>
+//           Hello {this.props.name}, you are {this.props.age} years old<br />
+//           So you were probably born {this.bornYear()}
+//         </p>
+//       </div>
+//     )
+//   }
+// }
+// const Hello = (props) => {
+//   return (
+//     <div>
+//       <p>Hello {props.name}, you are {props.age} years old</p>
+//     </div>
+//   )
+// }
 
 //testi
 const Footer = () => (

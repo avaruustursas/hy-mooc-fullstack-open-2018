db0b4e047d38ff4daf0db90e320dda7a48c1a1b2
avaruustursas <34242717+avaruustursas@users.noreply.github.com>
2018-08-21 03:21:25 +0300

    laskuri-esim talteen ennen kuin siirrytään tapahtumankäsittelijöihin

--- osa1/esimerkit/laskuri-esim/src/index.js
+++ osa1/esimerkit/laskuri-esim/src/index.js
@@ -2,14 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props)
+  constructor() {
+    super()
     this.state = {
       counter: 1
     }
+
+    setInterval(() => {
+      this.setState({counter: this.state.counter + 1})
+    }, 1000)
   }
 
   render() {
+    console.log('renderöidään',this.state.counter)
     return (
       <div>{this.state.counter}</div>
     )
